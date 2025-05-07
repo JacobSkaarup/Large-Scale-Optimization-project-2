c62c5d6f5c84b36fed3d021e9bbcc353b14d5db1
FrederikFertin <s203679@student.dtu.dk>
2025-05-07 08:11:16 +0200

    q4 initialization and stuff

---
+++ q4_setup.jl
@@ -0,0 +1,28 @@
+include("q4_data.jl")
+
+# Rows in the master problem
+masterRows = [13, 14, 15]
+
+# Rows in subproblems
+subBlocks=[[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12]]
+
+K = length(subBlocks) # Number of subproblems
+
+# Variables in subproblems
+V = Vector{Vector{Int64}}(undef, K)
+V[1] = [1, 2, 3, 7, 8, 9, 13, 14, 15]
+V[2] = [4, 5, 6, 10, 11, 12, 16, 17, 18]
+
+A0 = A[masterRows,:]
+b0 = b[masterRows,:]
+
+CV = Vector{Array{Float64,2}}(undef,K)
+A_V = Vector{Array{Float64,2}}(undef,K)
+A0_V = Vector{Array{Float64,2}}(undef,K)
+b_sub = Vector{Array{Float64,2}}(undef,K)
+for k=1:K
+    CV[k] = C[:,V[k]]
+    A_V[k] = A[subBlocks[k],V[k]]
+    A0_V[k] = A0[:,V[k]]
+    b_sub[k] = b[subBlocks[k],:]
+end
---
+++ q4_sub_it3.jl
@@ -0,0 +1,30 @@
+using JuMP, GLPK, LinearAlgebra
+
+include("q4_setup.jl")
+
+# dual variables from master problem:
+piVal = [0.0; -0.5; -0.2;;]
+kappa = [7.4, 8.0]
+
+for k=1:K
+    sub = Model(GLPK.Optimizer)
+
+    @variable(sub, y[1:Int(length(V[k])/3)], Bin ) # Binary variables for setup
+    @variable(sub, 0 <= x[1:Int(length(V[k])/3)] <= M2, Int ) # Integer variables for production
+    @variable(sub, 0 <= s[1:Int(length(V[k])/3)] <= M3, Int ) # Integer variables for storage
+    sub_variables = [x; y; s]
+
+    @objective(sub, Min, dot(CV[k], sub_variables) - dot(piVal' * A0_V[k], sub_variables) - kappa[k])
+    @constraint(sub, [con=1:3], dot(A_V[k][con,:], sub_variables) == b_sub[k][con] )
+    @constraint(sub, [con=4:6], dot(A_V[k][con,:], sub_variables) <= b_sub[k][con] )
+
+    optimize!(sub)
+
+    if termination_status(sub) == MOI.OPTIMAL
+        println("--- Result from sub-problem $k: ---")
+        println("Objective value: ", JuMP.objective_value(sub))
+        println("x: ", JuMP.value.(sub_variables))
+    else
+        println("Optimize was not succesful. Return code: ", termination_status(sub))
+    end
+end
